457a2a53c554deea21053225d5a2064498a15754
Bharadwaj-2024 <bharadwajbhadraroy@gmail.com>
2026-08-14 15:05:28 +0530

    Upgrade next.js to patch security vulnerabilities

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"0+gbPlB2YZKx+puzGCbEpXi/vMUI/RBlbW+t5cnIiYI=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
